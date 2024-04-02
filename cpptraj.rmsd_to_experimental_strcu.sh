5332b1ca0c254c388ca579d7c1c6d3c86ac356e1
HutchND <67545640+Niransha@users.noreply.github.com>
2024-04-02 16:07:41 -0400

    Create cpptraj.rmsd_pdb_combins_vs_experimental_strcu.sh

---
+++ cpptraj.rmsd_to_experimental_strcu.sh
@@ -0,0 +1,37 @@
+#SBATCH --job-name=cpprmsd
+#SBATCH -N 1
+#SBATCH -n 20 
+##SBATCH --gres=gpu:1
+##SBATCH --partition=shortq7,mediumq7,longq7,shortq7-gpu,longq7-rna
+#SBATCH --partition=longq7-rna
+##SBATCH --mail-type=ALL
+##SBATCH --time=6:00:00
+##SBATCH --exclusive
+##SBATCH --nodelist=nodegpu025
+
+#source /mnt/rna/home/programs/amber22/amber.sh
+source /mnt/rna/home/programs/amber22/amber22_athene.sh
+
+rm *.agr
+rm *.dat
+
+#find $SLURM_SUBMIT_DIR -maxdepth 1 -type f \( -name "slurm-*" ! -name "slurm-$SLURM_JOB_ID.out" \) -delete
+cat>input<<EOF
+parm ../top.prmtop 
+reference ../Analysis/ambGGCCadded.pdb
+
+for i=1;i<51;i++
+  #clear prevous trajin
+clear trajin    
+   trajin  ./pcca_\$i_50samples.pdb      
+symmrmsd \$i_ToExpHairpin4-7 :4-7&!@H= :4-7&!@H= reference out rmsd1.agr mass xlabel "acv "
+symmrmsd \$i_ToExpHairpin :4-7&!@H= :4-7&!@H= reference out rmsd1.dat mass xlabel "acv "
+
+done
+
+go
+EOF
+
+cpptraj.MPI -i input
+
+
